595958a28235f810a6bc9064f709a74f2186cb97
f8tum <parthnaik.office@gmail.com>
2026-03-08 12:17:28 +0530

    add transactions history render function

--- tracker.js
+++ tracker.js
@@ -1,12 +1,3 @@
-// Delete transaction on clicking the delete button
-const deleteBtns = document.querySelectorAll(".delete-transaction");
-
-deleteBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.closest(".history-card").remove();
-  });
-});
-
 // toggle income and expense buttons
 const typeBtns = document.querySelectorAll(".type-btn");
 
@@ -16,3 +7,60 @@ typeBtns.forEach(btn => {
     btn.classList.add("active");
   });
 });
+
+// ---------- Helper Functions ----------
+
+// update financials
+const renderTransaction = (transaction) => {
+  const card = document.createElement("div");
+  card.classList.add("history-card");
+
+  card.innerHTML = `
+    <div class="history-left">
+      <div class="history-left-icon">
+        <i class="fa-solid fa-circle ${transaction.type}"></i>
+      </div>
+
+      <div class="history-left-text">
+        <p class="description">${transaction.description}</p>
+        <p class="category">${transaction.category} • ${transaction.date}</p>
+      </div>
+    </div>
+
+    <div class="history-right">
+      <p class="money ${transaction.type}">
+        ${transaction.type === "income" ? "+" : "-"}₹${transaction.amount}
+      </p>
+      <p class="delete-transaction">
+        <i class="fa-solid fa-xmark"></i> delete
+      </p>
+    </div>
+  `;
+
+  // delete transaction
+  card.querySelector(".delete-transaction").addEventListener("click", () => {
+    card.remove();
+  });
+
+  document.querySelector(".history").appendChild(card);
+};  
+
+// take form data into an object
+const form = document.getElementById("entry-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent page reload
+
+  const transaction = {
+    type: document.querySelector(".type-btn.active").id === "incomeBtn" ? "income" : "expense",
+    description: document.getElementById("description").value,
+    amount: parseFloat(document.getElementById("amount").value),
+    date: document.getElementById("date").value,
+    category: document.getElementById("category").value,
+  };
+  console.log(transaction);
+
+  renderTransaction(transaction);
+
+  form.reset();
+}); 
